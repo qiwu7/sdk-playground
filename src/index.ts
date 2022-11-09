506b74cda6a207a7cd69c46ba52d96047e832c24
Qi Wu <qi.wu@coinbase.com>
2022-11-09 17:43:26 +0800

    Arbitrum redeem L2 retryable

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import { BigNumber, providers, Wallet } from "ethers";
-import {getL2Network, Erc20Bridger, L1ToL2MessageStatus} from "@arbitrum/sdk";
+import {getL2Network, Erc20Bridger, L1ToL2MessageStatus, L1TransactionReceipt} from "@arbitrum/sdk";
 import { Erc20DepositParams, L1ToL2TxReqAndSignerProvider } from "@arbitrum/sdk/dist/lib/assetBridger/erc20Bridger";
 import { isL1ToL2TransactionRequest, L1ToL2TransactionRequest } from "@arbitrum/sdk/dist/lib/dataEntities/transactionRequest";
 import { SignerProviderUtils } from "@arbitrum/sdk/dist/lib/dataEntities/signerOrProvider";
+import { L1ToL2MessageWaitResult } from "@arbitrum/sdk/dist/lib/message/L1ToL2Message";
 require('dotenv').config();
 
 const walletPrivateKey = process.env.PRIVKEY as string;
@@ -12,6 +13,44 @@ const l1Wallet = new Wallet(walletPrivateKey, l1Provider);
 const l2Wallet = new Wallet(walletPrivateKey, l2Provider);
 
 const main = async () => {
+    // const tokenDeposit = await deposit();
+    const r = await redeem("0x84831a641dd7877219b2e21121f3d6328f80ec07e20b5124b307125fc99528e8");
+};
+
+const redeem = async (txHash : string): Promise<string> => {
+    if (!txHash.startsWith('0x') || txHash.trim().length != 66) {
+        throw new Error(`invalid tx hash: ${txHash} `);
+      }
+  
+      const receipt = await l1Provider.getTransactionReceipt(txHash);
+      const l1Receipt = new L1TransactionReceipt(receipt);
+  
+      const messages = await l1Receipt.getL1ToL2Messages(l2Wallet);
+      const message = await messages[0];
+      const messageRec = await message.waitForStatus();
+      const status = await messageRec.status;
+      if (status === L1ToL2MessageStatus.REDEEMED) {
+        console.log(`L2 retryable txn is already executed`);
+        return "";
+      } else {
+        console.log(`L2 retryable txn failed with status ${L1ToL2MessageStatus[status]}`);
+      }
+      console.log(`Redeeming the ticket now`);
+      /**
+       * We use the redeem() method from Arbitrum SDK to manually redeem our ticket
+       */
+      const l2Tx = await message.redeem();
+      const l2Receipt = await l2Tx.waitForRedeem();
+      const l2TxHash = await l2Receipt.transactionHash;
+      console.log(
+          'The L2 side of your transaction is now execeuted:',
+          l2TxHash
+      );
+  
+      return l2TxHash;
+};
+
+const deposit = async (): Promise<L1ToL2TransactionRequest> => {
     const l2Network = await getL2Network(l2Provider);
     const erc20Bridge = new Erc20Bridger(l2Network);
     const erc20Address = "0x07865c6e87b9f70255377e024ace6630c1eaa37f";
@@ -24,12 +63,9 @@ const main = async () => {
         l2Provider: l2Provider,
     };
 
-    // console.log(walletPrivateKey);
-
     const erc20DepositParams = <Erc20DepositParams> params;
 
     const fromAddress = await l1Wallet.getAddress(); 
-    console.log(fromAddress);
     const l1p = SignerProviderUtils.getProviderOrThrow(params.l1Signer);
 
     const tokenDeposit = await erc20Bridge.getDepositRequest({
@@ -39,68 +75,7 @@ const main = async () => {
     });
 
     console.log(tokenDeposit);
-    console.log(tokenDeposit.txRequest.data);
-
-    // const r = await deposit(erc20Bridge, params);
-    // console.log(r);
-
-    // const approveTx = await erc20Bridge.approveToken({
-    //     l1Signer: l1Wallet,
-    //     erc20L1Address: erc20Address,
-    // })
-
-    // const approveRec = await approveTx.wait()
-    // console.log(
-    //     `You successfully allowed the Arbitrum Bridge to spend DappToken ${approveRec.transactionHash}`
-    // )
-
-    /**
-     * Deposit DappToken to L2 using erc20Bridge. This will escrow funds in the Gateway contract on L1, and send a message to mint tokens on L2.
-     * The erc20Bridge.deposit method handles computing the necessary fees for automatic-execution of retryable tickets — maxSubmission cost & l2 gas price * gas — and will automatically forward the fees to L2 as callvalue
-     * Also note that since this is the first DappToken deposit onto L2, a standard Arb ERC20 contract will automatically be deployed.
-     * Arguments required are:
-     * (1) amount: The amount of tokens to be transferred to L2
-     * (2) erc20L1Address: L1 address of the ERC20 token to be depositted to L2
-     * (2) l1Signer: The L1 address transferring token to L2
-     * (3) l2Provider: An l2 provider
-     */
-    // const depositTx = await erc20Bridge.deposit({
-    //     amount: tokenDepositAmount,
-    //     erc20L1Address: erc20Address,
-    //     l1Signer: l1Wallet,
-    //     l2Provider: l2Provider,
-    // });
-    // console.log(`deposit transaction sent ${depositTx}`);
-
-    // const depositRec = await depositTx.wait();
-    // const l2Result = await depositRec.waitForL2(l2Provider);
-    // console.log(`l2 result ${l2Result}`);
-
-    // /**
-    //  * The `complete` boolean tells us if the l1 to l2 message was successful
-    //  */
-    // l2Result.complete
-    //     ? console.log(
-    //         `L2 message successful: status: ${L1ToL2MessageStatus[l2Result.status]}`
-    //     )
-    //     : console.log(
-    //         `L2 message failed: status ${L1ToL2MessageStatus[l2Result.status]}`
-    //     );
-};
-
-const deposit = async (
-    erc20Bridger: Erc20Bridger,
-    params: Erc20DepositParams | L1ToL2TxReqAndSignerProvider
-): Promise<L1ToL2TransactionRequest> => {
-    const l1Provider = SignerProviderUtils.getProviderOrThrow(params.l1Signer)
-    const tokenDeposit = isL1ToL2TransactionRequest(params)
-        ? params
-        : await erc20Bridger.getDepositRequest({
-            ...params,
-            l1Provider,
-            from: await params.l1Signer.getAddress(),
-        })
-
+    // console.log(tokenDeposit.txRequest.data);
     return tokenDeposit;
 }
 
