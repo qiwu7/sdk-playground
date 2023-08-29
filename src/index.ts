b6f33d679aa7ab5d68be19f9d3fe088582817a44
Qi Wu <qi.wu@coinbase.com>
2023-08-30 01:52:01 +0800

    Use openzeppelin merkle tree

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { MerkleTreeWhitelist } from "./merkle_tree_whitelist";
+import { StandardMerkleTree } from "@openzeppelin/merkle-tree";
 
 const main = async () => {
     merkleTreeTest();
@@ -7,15 +7,18 @@ const main = async () => {
 function merkleTreeTest() {
     // Your whitelist from database
     const whitelist = [
-        '0xA9fedBeC3781c72fe8bbC2294C5070b4b5051fE9',
-        '0x83C2bbef5a09C4B46E049917a41E05fAf74b6275',
-        '0x7237D795b3983a9F220bFDda18A7f55d4bE8911f'
+        ['0xA9fedBeC3781c72fe8bbC2294C5070b4b5051fE9', 'speaker'],
+        ['0x83C2bbef5a09C4B46E049917a41E05fAf74b6275', 'speaker'],
+        ['0x7237D795b3983a9F220bFDda18A7f55d4bE8911f', 'attendee'],
+        ['0x0ecEEB86a43cbF87b58D571EFCD7215ACf5fF182', 'speaker'],
     ];
-    const mtw = new MerkleTreeWhitelist(whitelist);
 
-    const root = mtw.getMerkleRoot();
+    const tree = StandardMerkleTree.of(whitelist, ['address', 'string']);
+
+    console.log(tree.render());
+    const root = tree.root;
     console.log(root);
-    const proof =  mtw.generateProof("0xA9fedBeC3781c72fe8bbC2294C5070b4b5051fE9");
+    const proof =  tree.getProof(['0x0ecEEB86a43cbF87b58D571EFCD7215ACf5fF182', 'speaker']);
     console.log(proof);
 }
 
